59a87be28482b29df046630c486c54fdd07f989e
bogdanov <iceposhta@gmail.com>
2021-10-22 13:41:58 +0200

    add colors

---
+++ Catsgram/UI/Helpers/Color+Catsgram.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+public extension Color {
+    static let accentGreen = Color("accentGreen")
+    static let alertRed = Color("alertRed")
+    static let paleGrey = Color("paleGrey")
+    static let placeholderText = Color("placeholderText")
+    static let brownGrey = Color("brownGrey")
+}
---
+++ Catsgram/UI/LoginSignupView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginSignupView.swift
+//  Catsgram
+//
+//  Created by Oleksndr Bogdanov on 22.10.21.
+//
+
+import Foundation
