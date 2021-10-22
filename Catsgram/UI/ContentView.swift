550f7fae46ffc1287caab25a40ce09711903a978
bogdanov <iceposhta@gmail.com>
2021-10-22 15:00:00 +0200

    subscribe ContentView on signInPublisher _n_ signOutPublisher

--- Catsgram/UI/ContentView.swift
+++ Catsgram/UI/ContentView.swift
@@ -5,25 +5,42 @@
 //  Created by Oleksndr Bogdanov on 21.10.21.
 //
 
-import SwiftUI
 import Combine
+import SwiftUI
 
 struct ContentView: View {
+//    var publisher: AnyCancellable = {
+//        let client = APIClient()
+//        let request = PostRequest()
+//        return client.publisherForRequest(request)
+//            .sink { result in
+//                print(result)
+//            } receiveValue: { newPosts in
+//                print(newPosts)
+//            }
+//    }()
+    @State var showingLogin = true
 
-    var publisher: AnyCancellable = {
-        let client = APIClient()
-        let request = PostRequest()
-        return client.publisherForRequest(request)
-            .sink { result in
-                print(result)
-            } receiveValue: { newPosts in
-                print(newPosts)
-            }
-    }()
+    let signInPublisher = NotificationCenter.default
+        .publisher(for: .signInNotification)
+        .receive(on: RunLoop.main)
+
+    let signOutPublisher = NotificationCenter.default
+        .publisher(for: .signOutNofication)
+        .receive(on: RunLoop.main)
 
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .fullScreenCover(isPresented: $showingLogin) {
+                LoginSignupView()
+            }
+            .onReceive(signInPublisher) { _ in
+                showingLogin = false
+            }
+            .onReceive(signOutPublisher) { _ in
+                showingLogin = true
+            }
     }
 }
 
