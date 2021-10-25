ded186b4b7ffd1ef7ca76833ce022ffddbf033d2
bogdanov <iceposhta@gmail.com>
2021-10-25 16:00:40 +0200

    implemented a Feed model

---
+++ Catsgram/Service Layer/Models/Feed.swift
@@ -0,0 +1,35 @@
+//
+//  Feed.swift
+//  Catsgram
+//
+//  Created by Oleksndr Bogdanov on 25.10.21.
+//
+
+import Combine
+import Foundation
+
+class Feed: ObservableObject {
+    @Published var posts: [Post] = []
+    @Published var loadError: Error?
+    var signInSubscriber: AnyCancellable?
+    var getPostSubscriber: AnyCancellable?
+
+    init() {
+        signInSubscriber = NotificationCenter.default.publisher(for: .signInNotification).sink { [weak self] _ in
+            self?.loadFeed()
+        }
+    }
+
+    func loadFeed() {
+        let client = APIClient()
+        let request = PostRequest()
+        getPostSubscriber = client.publisherForRequest(request)
+            .sink { [weak self] result in
+                if case .failure(let error) = result {
+                    self?.loadError = error
+                }
+            } receiveValue: { [weak self] newPosts in
+                self?.posts = newPosts
+            }
+    }
+}
