8b89c7acb2153ee194c7a304a810ee55b5f74ee7
bogdanov <iceposhta@gmail.com>
2021-10-29 17:16:56 +0200

    add Swifter to mock user login

--- Catsgram/Supporting Files/CatsgramApp.swift
+++ Catsgram/Supporting Files/CatsgramApp.swift
@@ -5,10 +5,24 @@
 //  Created by Oleksndr Bogdanov on 21.10.21.
 //
 
+import Swifter
 import SwiftUI
 
 @main
 struct CatsgramApp: App {
+    let server: HttpServer = {
+        let server = HttpServer()
+        try? server.start(8081)
+        server.POST["/api/v1/user"] = { _ in HttpResponse.ok(.text("""
+        {
+          "username": "username",
+          "email": "email@example.com"
+        }
+        """)) }
+        server.post["/api/v1/image"] = { _ in HttpResponse.ok(.text("")) }
+        return server
+    }()
+
     var body: some Scene {
         WindowGroup {
             MainView()
--- Catsgram/UI/Post/PostController.swift
+++ Catsgram/UI/Post/PostController.swift
@@ -19,6 +19,7 @@ final class PostController: ObservableObject {
             }
             .flatMap { (imageId, imageData) -> AnyPublisher<Void, Error> in
                 let imageRequest = UploadImageRequest(imageId: imageId, imageData: imageData)
+                let localClient = APIClient(environment: .local81)
                 return client.publisherForRequest(imageRequest)
             }
             .sink(receiveCompletion: { completion in
